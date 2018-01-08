e5bc85913d3852a8849dcf9dab985ece681ff235
Grzegorz Przybyła <grzegorz.przybyla@icloud.com>
2018-01-08 17:53:25 +0100

    Add app icon, to see how to use the assets catalog,
    App still crash

--- CardWallet/CardWallet/HomeViewController.swift
+++ CardWallet/CardWallet/HomeViewController.swift
@@ -11,15 +11,12 @@ import UIKit
 class HomeViewController: UIViewController {
 
     
- //   @IBOutlet weak var table: UITableView!
+    @IBOutlet weak var tableView: UITableView!
     
   //  var cards : [String] = [""]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-   //     table.tableFooterView = UIView(frame: CGRect.zero)
-
-        // Do any additional setup after loading the view.
     }
 
 
@@ -41,16 +38,5 @@ class HomeViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
