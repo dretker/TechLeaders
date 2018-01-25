9d77e5e5a3fe0e5317ada137a367d6f5e7c32ae0
Grzegorz Przybyła <grzegorz.przybyla@icloud.com>
2018-01-25 08:14:15 +0100

    Remove unused IBOutlet, added AddCard button

--- CardWallet/CardWallet/UIViewControllers/CardsTableViewController.swift
+++ CardWallet/CardWallet/UIViewControllers/CardsTableViewController.swift
@@ -17,6 +17,10 @@ class CardsTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         loadSampleCards()
+        title = "Your cards"
+        let addCardButton = navigationItem.rightBarButtonItem
+        addCardButton?.target = self
+        addCardButton?.action = #selector(didTapAddCardButton(_:))
     }
 
     override func didReceiveMemoryWarning() {
@@ -54,51 +58,9 @@ class CardsTableViewController: UITableViewController {
         return cell
     }
  
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    @objc private func didTapAddCardButton(_ sender: AnyObject) {
+        print("Add card")
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
     
     
     
